5a184d129fa85278c4e41d389c5b449de05ab81a
edi <128224911+edi-jr@users.noreply.github.com>
2023-06-11 11:00:48 -0300

    Add edit and delete book functions

--- script.js
+++ script.js
@@ -11,15 +11,28 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+function editBook(e) {
+  myLibrary[currentEditIndex].title = editForm.querySelector("#title").value;
+  myLibrary[currentEditIndex].author = editForm.querySelector("#author").value;
+  myLibrary[currentEditIndex].pages = editForm.querySelector("#pages").value;
+  myLibrary[currentEditIndex].isRead = editForm.querySelector("#isRead").checked;
+}
+
+function removeBookFromLibrary(e) {
+  myLibrary.splice(e.target.dataset.index, 1);
+  displayBooks();
+}
+
 function getBook() {
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const pages = document.querySelector("#pages").value;
-  const isRead = document.querySelector("#isRead").checked;
+  const title = addForm.querySelector("#title").value;
+  const author = addForm.querySelector("#author").value;
+  const pages = addForm.querySelector("#pages").value;
+  const isRead = addForm.querySelector("#isRead").checked;
   return new Book(title, author, pages, isRead);
 }
 
 function createBookElement(book) {
+  book.index = myLibrary.indexOf(book);
   const tr = document.createElement("tr");
   const title = document.createElement("td");
   const author = document.createElement("td");
@@ -31,7 +44,11 @@ function createBookElement(book) {
   const editBtn = document.createElement("button");
   const deleteBtn = document.createElement("button");
   editBtn.innerHTML = "Edit";
+  editBtn.dataset.index = book.index;
+  editBtn.addEventListener("click", e => openEditModal(e));
   deleteBtn.innerHTML = "Delete";
+  deleteBtn.dataset.index = book.index;
+  deleteBtn.addEventListener("click", e => removeBookFromLibrary(e));
   title.innerHTML = book.title;
   author.innerHTML = book.author;
   pages.innerHTML = book.pages;
@@ -52,30 +69,56 @@ function displayBooks() {
   myLibrary.forEach(book => createBookElement(book));
 }
 
-function openModal() {
-  modalContainer.style.display = "block";
+function openEditModal(e) {
+  modalsContainer.style.display = "block";
+  editModal.style.display = "block";
+  currentEditIndex = e.target.dataset.index;
+  editForm.querySelector("#title").value = myLibrary[currentEditIndex].title;
+  editForm.querySelector("#author").value = myLibrary[currentEditIndex].author;
+  editForm.querySelector("#pages").value = myLibrary[currentEditIndex].pages;
+  editForm.querySelector("#isRead").checked = myLibrary[currentEditIndex].isRead;
 }
 
-function closeModal() {
-  form.reset();
-  modalContainer.style.display = "none";  
+function openAddModal() {
+  modalsContainer.style.display = "block";
+  addModal.style.display = "block";
 }
 
-function handleSubmit(e) {
+function closeModals() {
+  forms.forEach(form => form.reset());
+  modalsContainer.style.display = "none";
+  addModal.style.display = "none";
+  editModal.style.display = "none";
+}
+
+function handleAddSubmit(e) {
   e.preventDefault();
   const book = getBook();
   addBookToLibrary(book);
-  closeModal();
+  closeModals();
+  displayBooks();
+}
+
+function handleEditSubmit(e) {
+  e.preventDefault();
+  editBook();
+  closeModals();
   displayBooks();
 }
 
 const table = document.querySelector("tbody");
 const newBookBtn = document.querySelector(".newBookBtn");
-const modalContainer = document.querySelector(".modal-container");
+const modalsContainer = document.querySelector(".modals-container");
+const addModal = document.querySelector(".add-modal");
+const addForm = document.querySelector("#add-form");
+const editModal = document.querySelector(".edit-modal");
+const editForm = document.querySelector("#edit-form");
 const closeModalElements = document.querySelectorAll(".close");
-const form = document.querySelector("#form");
+const forms = document.querySelectorAll(".form");
+let currentEditIndex = 0;
 
-newBookBtn.addEventListener("click", openModal);
-closeModalElements.forEach(element => element.addEventListener("click", closeModal));
-window.addEventListener("click", e => e.target === modalContainer ? closeModal() : "");
-form.addEventListener("submit", e => handleSubmit(e));
+newBookBtn.addEventListener("click", openAddModal);
+closeModalElements.forEach(element => element.addEventListener("click", closeModals));
+window.addEventListener("click", e => e.target === modalsContainer ? closeModals() : "");
+addForm.addEventListener("submit", e => handleAddSubmit(e));
+editForm.addEventListener("submit", e => handleEditSubmit(e));
